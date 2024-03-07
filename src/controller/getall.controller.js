b300547e6bab681d4c83d7fbdcc5a56bbf15bdd5
xertifiedloaded <certifiedloaded@yahoo.com>
2024-03-07 23:48:55 +0100

    my changes

--- src/controller/getall.controller.js
+++ src/controller/getall.controller.js
@@ -1,16 +1,18 @@
+
 import Portfolio from "../Schema/data.js";
 import { errorResMsg, successResMsg } from "../middleware/errorHandler.js";
 
 const getAllPortfolioEntries = async (req, res) => {
   try {
-    const portfolioEntries = await Portfolio.find();
+    // Fetch portfolio entries sorted by creation date in descending order
+    const portfolioEntries = await Portfolio.find().sort({ createdAt: -1 });
+
     return successResMsg(res, 200, {
-      message: "all portfolio fetched",
+      message: "All portfolio entries fetched",
       portfolioEntries,
     });
   } catch (error) {
-   return  errorResMsg(res,500,"error fetching portfolio")
-
+    return errorResMsg(res, 500, "Error fetching portfolio entries");
   }
 };
 
